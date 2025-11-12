b3eaa2ec708b50eaaf78b7464656d546998ae25f
Artur <artureco95.af@gmail.com>
2025-11-12 23:55:53 +0000

    load_dotenv is back for local dev

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 # load_dotenv() will automatically search for and load variables from a .env file
 # in the current directory or parent directories.
-#load_dotenv()
+load_dotenv()
 
 # --- Telegram Configuration ---
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
